55db35a25396b5a2c8cdfb5538385ecf881d0e78
FenriRagni <145153161+FenriRagni@users.noreply.github.com>
2023-10-28 03:13:56 -0400

    add initial connection to db

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 const inquirer = require('inquirer');
+const mysql = require('mysql2');
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'employee_db'
+    },
+    console.log(`Connected to the employee_db database.`)
+)
+
+db.query(`SELECT *`, function (err, results){
+    if(results){
+        console.log(results);
+    }
+    else{
+        console.log("no response returned");
+    }
+})
