7fe76acebbb987d1493012bbde1d0366895a0337
FenriRagni <145153161+FenriRagni@users.noreply.github.com>
2023-10-31 17:25:05 -0400

    add show employee query

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const Table = require('cli-table');
-const {addDepartment, displayDepartment}= require('./lib/department.js');
 
 const db = mysql.createConnection(
     {
@@ -27,27 +26,60 @@ function init(){
         .then((results) => {
             switch(results.choice){
                 case 'view all departments': 
-                db.query("SELECT department_name FROM department", function (err, res) {
-                    console.log(res);
-                    if(!err){
+                db.query("SELECT * FROM department", function (err, res) {
+                    if(res){
                         let table = new Table({
-                            head: ["Departments"],
-                            colWidths: [20]
+                            head: ["Dept_id","Departments"],
+                            colWidths: [10, 20]
                         });
                         for(let i = 0; i < res.length; i++){
-                            table.push([res[i].department_name]);
+                            table.push([res[i].id, res[i].department_name]);
                         }
                         console.log(table.toString());
                         init();
                     }
                     else{
-                        return err;
+                        console.log("No departments found");
+                        init();
                     }
                 });
                 break;
                 case 'view all roles':
+                db.query("SELECT * FROM role", function (err, res) {
+                    if(res){
+                        let table = new Table({
+                            head: ["Role_id", "Title", "Salary", "Dept_id"],
+                            colWidths: [10, 20, 10, 10]
+                        });
+                        for(let i = 0; i < res.length; i++){
+                            table.push([res[i].id, res[i].title, res[i].salary, res[i].department_id]);
+                        }
+                        console.log(table);
+                        console.log(table.toString());
+                        init();
+                    }
+                    else{
+                        console.log("No roles found");
+                        init();
+                    }
+                });
                 break;
                 case 'view all employees':
+                    let table = new Table({
+                        head: ["employee_id", "first_name", "last_name", "role", "department", "salary", "manager"],
+                        colWidths: [10, 10, 10, 10, 10, 10, 10]
+                    });
+                    db.query('SELECT a.id, a.first_name, a.last_name, role.title, department.department_name, role.salary, CONCAT_WS(" ", b.first_name, b.last_name) AS supervisor FROM employee a INNER JOIN role ON a.role_id = role.id INNER JOIN department ON role.department_id = department.id LEFT JOIN employee b ON a.manager_id = b.id" = role.id', function (err, res) {
+                        if(res){
+                            for(let i = 0; i < res.length; i++){
+                                table.push([res[i].id, res[i].first_name, res[i].last_name, res[i].title, res[i].department_name, res[i].salary, res[i].supervisor]);
+                            }
+                        }
+                        else{
+                            console.log("No employees found");
+                            init();
+                        }
+                    });
                 break;
                 case 'add a department': 
                 break;
