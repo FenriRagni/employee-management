7b74735958dac9aceb1abced6174abf077d347b4
FenriRagni <145153161+FenriRagni@users.noreply.github.com>
2023-10-31 23:57:33 -0400

    updated add role function to allow choice from list rather than needing id

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function init(){
             message: 'Choose an option',
             choices: ['view all departments','view all roles', 'view all employees',
             'add a department', 'add a role', 'add an employee',
-            'update employee role'],
+            'update employee role','quit'],
             loop: 'true'
         }])
         .then((results) => {
@@ -33,13 +33,15 @@ function init(){
                 break;
                 case 'add a department': addDepartment();
                 break;
-                case 'add a role':
+                case 'add a role': addRole();
                 break;
-                case 'add an employee':
+                case 'add an employee': addEmployee();
                 break;
                 case 'update employee role':
                 break;
+                case'quit': process.exit();
             }
+            
         });
 }
 
@@ -48,6 +50,7 @@ init();
 function displayDepartment() {
     db.query("SELECT * FROM department", function (err, res) {
         if(res){
+            console.log(res);
             let table = new Table({
                 head: ["Dept_id","Departments"],
                 colWidths: [10, 20]
@@ -55,7 +58,7 @@ function displayDepartment() {
             for(let i = 0; i < res.length; i++){
                 table.push([res[i].id, res[i].department_name]);
             }
-            console.log(table.toString());
+            console.log("\n" + table.toString());
             return init();
         }
         else{
@@ -75,7 +78,7 @@ function displayRole() {
             for(let i = 0; i < res.length; i++){
                 table.push([res[i].id, res[i].title, res[i].salary, res[i].department_id]);
             }
-            console.log(table.toString());
+            console.log("\n" + table.toString());
             return init();
         }
         else{
@@ -95,7 +98,7 @@ function displayEmployees() {
             for(let i = 0; i < res.length; i++){
                 table.push([res[i].id, res[i].first_name, res[i].last_name, res[i].title, res[i].department_name, res[i].salary, res[i].supervisor]);
             }
-            console.log(table.toString());
+            console.log("\n" + table.toString());
             return init();
         }
         else{
@@ -107,16 +110,76 @@ function displayEmployees() {
 
 function addDepartment(){
     inquirer.prompt([{name: "dept", message: "What is the name of the Department?", }])
-            .then((results) => {
-                db.query('INSERT INTO department(department_name) VALUES (?)', results.dept, function (err, res){
-                    if(res){
-                        return displayDepartment();
-                        
-                    }
-                    else{
-                        console.log("Unable to add employee");
-                        return init();
-                    }
-                })
+    .then((results) => {
+        db.query('INSERT INTO department(department_name) VALUES (?)', results.dept, function (err, res){
+            if(res){
+                console.log(`Added ${results.dept} to database`);
+                return init();
+                
+            }
+            else{
+                console.log("Unable to add department");
+                return init();
+            }
+        })
+    })
+}
+
+function addRole() {
+    db.query("SELECT * FROM department", function (err, res){
+        let dept = [];
+        for(let x = 0; x < res.length; x++){
+            dept.push(res[x].department_name);
+        }
+        inquirer.prompt(
+            [
+                {name: "title", message: "What is the role title?"}, 
+                {name: "salary", message: "What is the role's salary?"},
+                {type: "list", name: "department_id", message: "What department does this role belong to?", choices: dept}
+            ])
+        .then((results) => {
+            db.query('INSERT INTO role(title, salary, department_id) VALUES (? , ? , ?)', 
+            [results.title, parseFloat(results.salary), parseInt(res[dept.indexOf(results.department_id)+1].id)], function (err, res){
+                if(res){
+                    console.log(`Added ${results.title} role to database`)
+                    return displayRole();  
+                }
+                else{
+                    console.log("Unable to add role");
+                    return init();
+                }
             })
+        })
+    })
+    
+}
+
+function addEmployee(){
+    inquirer.prompt(
+        [
+            {name: "first", message: "Employee's first name: "},
+            {name: "last", message: "Employee's last name: "},
+            {name: "role", message: "Employee's role id: "},
+            {name: "manager", message: "Employee's manager's id: "}
+        ]
+    )
+    .then((results) => {
+        db.query('INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES (?, ? , ?, ?)',
+        [results.first, results.last, parseInt(results.role), parseInt(results.manager)], function(err, res){
+            if(res){
+                return displayEmployees();
+            }
+            else{
+                console.log("Unable to add employee");
+                return init();
+            }
+        })
+    })
+}
+
+function updateEmployee() {
+    inquirer.prompt(
+        [
+            {name: "id", message: ""}
+        ])
 }
