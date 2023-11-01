b183f0f83d222589a01e155575e36fc631876da0
FenriRagni <145153161+FenriRagni@users.noreply.github.com>
2023-10-31 22:18:47 -0400

    cleaned up functions, add addDepartment function

--- index.js
+++ index.js
@@ -25,64 +25,13 @@ function init(){
         }])
         .then((results) => {
             switch(results.choice){
-                case 'view all departments': 
-                db.query("SELECT * FROM department", function (err, res) {
-                    if(res){
-                        let table = new Table({
-                            head: ["Dept_id","Departments"],
-                            colWidths: [10, 20]
-                        });
-                        for(let i = 0; i < res.length; i++){
-                            table.push([res[i].id, res[i].department_name]);
-                        }
-                        console.log(table.toString());
-                        init();
-                    }
-                    else{
-                        console.log("No departments found");
-                        init();
-                    }
-                });
+                case 'view all departments': displayDepartment();
                 break;
-                case 'view all roles':
-                db.query("SELECT * FROM role", function (err, res) {
-                    if(res){
-                        let table = new Table({
-                            head: ["Role_id", "Title", "Salary", "Dept_id"],
-                            colWidths: [10, 20, 10, 10]
-                        });
-                        for(let i = 0; i < res.length; i++){
-                            table.push([res[i].id, res[i].title, res[i].salary, res[i].department_id]);
-                        }
-                        console.log(table.toString());
-                        init();
-                    }
-                    else{
-                        console.log("No roles found");
-                        init();
-                    }
-                });
+                case 'view all roles': displayRole();
                 break;
-                case 'view all employees':
-                    let table = new Table({
-                        head: ["Employee ID", "First Name", "Last Name", "Role", "Department", "Salary", "Manager"],
-                        colWidths: [15, 15, 15, 15, 15, 15, 15]
-                     });
-                    db.query('SELECT a.id, a.first_name, a.last_name, role.title, department.department_name, role.salary, IFNULL(CONCAT(b.first_name," ", b.last_name),"null") AS supervisor FROM employee a INNER JOIN role ON a.role_id = role.id INNER JOIN department ON role.department_id = department.id LEFT JOIN employee b ON a.manager_id = b.id', function (err, res) {
-                        if(res){
-                            for(let i = 0; i < res.length; i++){
-                                table.push([res[i].id, res[i].first_name, res[i].last_name, res[i].title, res[i].department_name, res[i].salary, res[i].supervisor]);
-                            }
-                            console.log(table.toString());
-                            init();
-                        }
-                        else{
-                            console.log("No employees found");
-                            init();
-                        }
-                    });
+                case 'view all employees' : displayEmployees();
                 break;
-                case 'add a department': 
+                case 'add a department': addDepartment();
                 break;
                 case 'add a role':
                 break;
@@ -95,3 +44,79 @@ function init(){
 }
 
 init();
+
+function displayDepartment() {
+    db.query("SELECT * FROM department", function (err, res) {
+        if(res){
+            let table = new Table({
+                head: ["Dept_id","Departments"],
+                colWidths: [10, 20]
+            });
+            for(let i = 0; i < res.length; i++){
+                table.push([res[i].id, res[i].department_name]);
+            }
+            console.log(table.toString());
+            return init();
+        }
+        else{
+            console.log("No departments found");
+            return init();
+        }
+    });
+}
+
+function displayRole() {
+    db.query("SELECT * FROM role", function (err, res) {
+        if(res){
+            let table = new Table({
+                head: ["Role_id", "Title", "Salary", "Dept_id"],
+                colWidths: [10, 20, 10, 10]
+            });
+            for(let i = 0; i < res.length; i++){
+                table.push([res[i].id, res[i].title, res[i].salary, res[i].department_id]);
+            }
+            console.log(table.toString());
+            return init();
+        }
+        else{
+            console.log("No roles found");
+            return init();
+        }
+    });
+}
+
+function displayEmployees() {
+    db.query('SELECT a.id, a.first_name, a.last_name, role.title, department.department_name, role.salary, IFNULL(CONCAT(b.first_name," ", b.last_name),"null") AS supervisor FROM employee a INNER JOIN role ON a.role_id = role.id INNER JOIN department ON role.department_id = department.id LEFT JOIN employee b ON a.manager_id = b.id', function (err, res) {
+        if(res){
+            let table = new Table({
+                head: ["Employee ID", "First Name", "Last Name", "Role", "Department", "Salary", "Manager"],
+                colWidths: [15, 15, 15, 15, 15, 15, 15]
+            });
+            for(let i = 0; i < res.length; i++){
+                table.push([res[i].id, res[i].first_name, res[i].last_name, res[i].title, res[i].department_name, res[i].salary, res[i].supervisor]);
+            }
+            console.log(table.toString());
+            return init();
+        }
+        else{
+            console.log("No employees found");
+            return init();
+        }
+    });
+}
+
+function addDepartment(){
+    inquirer.prompt([{name: "dept", message: "What is the name of the Department?", }])
+            .then((results) => {
+                db.query('INSERT INTO department(department_name) VALUES (?)', results.dept, function (err, res){
+                    if(res){
+                        return displayDepartment();
+                        
+                    }
+                    else{
+                        console.log("Unable to add employee");
+                        return init();
+                    }
+                })
+            })
+}
